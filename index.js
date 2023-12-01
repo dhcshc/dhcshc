193f890750670da245661a1742014be79496d4e6
dhcshc <dhcshc@naver.com>
2023-12-01 15:08:14 +0900

    update price

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const express = require('express')
+import express from 'express'
+import fetch from 'node-fetch'
+import dateFormat from 'dateformat'
+
+
 const app = express()
 const port = 3000
 
+
+async function getPriceByJson(ticker) {
+    const count = 1
+    const today = dateFormat(new Date(), 'yyyymmdd')
+    const url = `https://api.finance.naver.com/siseJson.naver?symbol=${ticker}&requestType=2&count=${count}&startTime=${today}&timeframe=day`
+    const resp = await fetch(url)
+    const body = await resp.text()
+    const text = body.replace(/'/g, '"').replace(/\s/g, "")
+    const arr = eval(text)
+    return arr[1][4]
+}
+
+
 app.get('/', (req, res) => {
   res.send('Hello World')
+})
+
+
+app.get('/price/:ticker', (req, res) => {
+  const { ticker } = req.params
+  getPriceByJson(ticker)
+    .then(price => {
+      console.log(price)
+      res.json({'price': price})
+    })
 })
 
 app.get('/user/:name', (req, res) => {
